1451a9e5b6f707a746569354ff1c80e3bb4d6fea
Shirokova <shirokovavaleria@gmail.com>
2021-11-04 23:54:03 +0300

    Python

---
+++ Python/pythonHomework1.py
@@ -0,0 +1,56 @@
+var = "Valeria"
+var_type = type(var)
+print("Var:", var)
+print("Var_type:", var_type)
+
+var = 3
+var_type = type(var)
+print("Var:", var)
+print("Var_type:", var_type)
+
+var = 0.5
+var_type = type(var)
+print("Var:", var)
+print("Var_type:", var_type)
+
+var = bytes(11)
+var_type = type(var)
+print("Var:", var)
+print("Var_type:", var_type)
+
+var = ["Valeria", 3, 0.5, True]
+var_type = type(var)
+print("Var:", var)
+print("Var_type:", var_type)
+
+var = ("Valeria", 3, 0.5, True)
+var_type = type(var)
+print("Var:", var)
+print("Var_type:", var_type)
+
+var = set("Valeria")
+var_type = type(var)
+print("Var:", var)
+print("Var_type:", var_type)
+
+var = frozenset("Valeria")
+var_type = type(var)
+print("Var:", var)
+print("Var_type:", var_type)
+
+var = {"key": 1, "value": 2}
+var_type = type(var)
+print("Var:", var)
+print("Var_type:", var_type)
+
+a = "Valeria"
+b = "Shirokova"
+c = a + b
+print(c)
+
+d = "lesson"
+e = 1
+print(d, e)
+print(d + str(e))
+
+
